8c44b2fdbfae57c6f0e0380adeac55f1d2ca10c4
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-09-10 12:48:16 +0200

    add move type and example game

--- dev.md
+++ dev.md
@@ -1,2 +1,6 @@
-- [ ] types and example game
+- [x] types and example game
 - [ ] interface for two human players
+
+nice to haves:
+- [ ] better ascii representation
+- [ ] highlight lastmove
--- tacumitac.nim
+++ tacumitac.nim
@@ -6,6 +6,7 @@ type
   Cell* = enum
     Empty = " ", X, O
   Grid* = array[Position, Cell]
+  Move* = tuple[pos: Position, val: Cell]
 
 func `$`*(g: Grid): string =
   $g[NW] & '|' & $g[N] & '|' & $g[NE] & '\n' &
@@ -19,3 +20,17 @@ func newGame*: Grid = result
 when isMainModule:
   var g = newGame()
   echo g
+  let exampleGame = [
+    (C, X).Move,
+    (NE, O),
+    (E, X),
+    (W, O),
+    (S, X),
+    (N, O),
+    (NW, X),
+    (SE, O),
+    ]
+  for i, move in exampleGame:
+    echo "\nMove " & $i & ":\n"
+    g[move.pos] = move.val
+    echo g
